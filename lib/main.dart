cf0ed119e7ec8fe4037e3907e8b3d234f33a4cd8
fathimathsafa <safahaneefa01@gmail.com>
2026-09-09 18:15:29 +0530

    NAME changed

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aurify Task',
+      title: 'AURIFY',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
       themeMode: ThemeMode.light,
